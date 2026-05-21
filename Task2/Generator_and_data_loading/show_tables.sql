075c2574ddbf5d7c60dd5612c454a12075f3bcfa
Maciej Kowalczyk <mkowalczyk0304@gmail.com>
2026-05-21 16:20:00 +0200

    Add files via upload

---
+++ Task2/Generator_and_data_loading/show_tables.sql
@@ -0,0 +1,20 @@
+USE FitnessClubT1;
+GO
+
+SELECT * FROM Sala;
+GO
+
+SELECT * FROM TypZajec;
+GO
+
+SELECT * FROM Instruktor;
+GO
+
+SELECT * FROM Czlonek;
+GO
+
+SELECT * FROM Zajecia;
+GO
+
+SELECT * FROM Zapis;
+GO
